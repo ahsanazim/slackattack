2eb4b3b0dfd9a0b8805c0c312bdc14a98fb600a1
AhsanAzim <ahsan-azim@live.com>
2016-07-09 23:56:36 -0400

    basic reqs filled

--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
+// ------------------------- initialization ------------------------------------
+
 import botkit from 'botkit';
 // this is es6 syntax for importing libraries
 // in older js this would be: var botkit = require('botkit')
@@ -5,9 +7,7 @@ import botkit from 'botkit';
 // Yelp
 const Yelp = require('yelp');
 
-// checking whether a specific case is fulfilled
-// let match = false;
-
+// yelp object, when need yelp will act on this
 const yelp = new Yelp({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -17,12 +17,6 @@ const yelp = new Yelp({
 
 // at startup
 console.log('starting bot');
-
-// See http://www.yelp.com/developers/documentation/v2/business
-// SAMPLE YELP CODE:
-// yelp.business('yelp-san-francisco')
-//   .then(console.log)
-//   .catch(console.error);
 
 // botkit controller
 const controller = botkit.slackbot({
@@ -46,7 +40,9 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
-// example hello response
+// -----------------------------------------------------------------------------
+
+// hello response
 controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (res) {
@@ -57,12 +53,12 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
-// outgoing webhook, replies when mentioned as @ahsan_bot
+// outgoing webhook to wake up bot, replies when mentioned as @ahsan_bot
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'i\'m awakeeeee http://giphy.com/gifs/lol-star-wars-7zdsOWDgSCzDi');
 });
 
-// default reply, triggered when no specific case fulfilled
+// help required
 controller.hears('help', ['direct_mention', 'mention', 'direct_message'], (bot, message) => {
   console.log('sending help message');
   bot.reply(message, 'Hi, my name is golem (short for Self-Reliant Riot' +
@@ -70,6 +66,78 @@ controller.hears('help', ['direct_mention', 'mention', 'direct_message'], (bot, 
                       ' bot, except better. I can do so much and so much' +
                       ' more. Here are a few things I can help you with: ');
 });
+
+// ------------------------ Yelp food conversation -----------------------------
+
+controller.hears('hungry', ['direct_mention', 'mention', 'direct_message'], (bot, message) => {
+  bot.startConversation(message, askWantFood);
+});
+
+function askWantFood(response, convo) {
+  convo.ask(':pizza:Would you like food recommendations nearby?', () => {
+    convo.say('Awesome.');
+    askTypeFood(response, convo);
+    convo.next();
+  });
+}
+
+function askTypeFood(response, convo) {
+  const foodKindResp = { key: 'foodKind', multiple: false };
+  convo.ask('What kind of food do you like?', () => {
+    convo.say('Ok.');
+    askLocationFood(response, convo);
+    convo.next();
+  }, foodKindResp);
+}
+
+function askLocationFood(response, convo) {
+  const locationResp = { key: 'foodLocation', multiple: false };
+  convo.ask('So where do you live?', () => {
+    convo.say('Ok! Give me a second . . .');
+    tellFoodPlaces(response, convo);
+    convo.next();
+  }, locationResp);
+}
+
+function tellFoodPlaces(response, convo) {
+  const foodKindStr = convo.extractResponse('foodKind');
+  const foodLocationStr = convo.extractResponse('foodLocation');
+  convo.say('Here are the 10 best results: ');
+  // See http://www.yelp.com/developers/documentation/v2/search_api
+  yelp.search({ term: `food+${foodKindStr}`, location: `${foodLocationStr}`, category_filter: 'food,restaurants', sort: 1, limit: 10 })
+  .then((data) => {
+    console.log(data);
+    data.businesses.forEach(business => {
+      // do something with business
+      const replyWithAttachments = {
+        username: '',
+        text: '',
+        attachments: [
+          {
+            fallback: 'Oops ... this restaurant doesn\'t seem available',
+            pretext: '',
+            title: `:yum: ${business.name}`,
+            title_link: `${business.mobile_url}`,
+            text: `rating: ${business.rating}\n${business.snippet_text}\n` +
+                  `:phone: ${business.display_phone}`,
+            image_url: `${business.image_url}`,
+            color: '#7CD197',
+            unfurl_media: true,
+            unfurl_links: true,
+          },
+        ],
+        icon_url: '',
+      };
+
+      convo.say(replyWithAttachments);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+// -----------------------------------------------------------------------------
 
 // default reply, triggered when no specific case fulfilled
 controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
